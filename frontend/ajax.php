d76cfd33960922ac3db00dcad428807f902a6552
elieish <elieish@gmail.com>
2013-07-29 16:49:02 +0200

    hashed password

---
+++ frontend/ajax.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ *Project
+ * @author Elie Ishimwe <elieish@gmail.com>
+ * @version 1.0
+ * @package Project 
+ */
+ 
+# ===================================================
+# SCRIPT SETTINGS
+# ===================================================
+
+# Start Session
+session_start();
+
+# Include Required Scripts
+include_once(dirname(dirname(__FILE__)) . "/backend/framework/include.php");
+Application::include_models();
+Application::include_helpers();
+
+# ===================================================
+# FUNCTIONS
+# ===================================================
+
+function save_signup_entry() {
+    global $_db, $validator;
+	
+	
+	
+ 	$uid            = $validator->validate($_GET['uid'],   "Integer");
+    $email          = $validator->validate($_GET['email'], "String");
+    $pass			= $validator->validate($_GET['cpass'], "String"); 
+	$fname			= $validator->validate($_GET['fname'], "String");
+	$lname		 	= $validator->validate($_GET['lname'], "String");
+	$phone 			= $validator->validate($_GET['phone'], "String");
+  	$address 		= $validator->validate($_GET['address'],"Integer");
+        if ($uid > 0) {
+         
+         $_db->update(
+                    '',
+                    array(
+                        
+                                              
+                    ),
+                    array(
+                        'uid' 			=> $uid
+                    )
+                ); 
+				
+				echo "OK";
+        } 
+        else {
+         $_db->insert(
+                'users',
+                array(
+					'creation_date'	=> now(),
+                    'username'   	=> $email,
+                    'user_type_id'  => 1,
+                    'password'  	=> md5($pass),
+                    'first_name'	=> $fname,
+                    'last_name'		=> $lname,
+                    'mobile'		=> $phone,    
+                    'active'        => 1
+                )
+            ); 
+       
+       
+      echo "OK";
+    }
+}
+
+
+
+
+# ===================================================
+# ACTION HANDLER
+# ===================================================
+
+if (isset($_GET['action'])) {
+	$action																= $_GET['action'];
+	if (function_exists($action)) {
+		$action();
+	}
+	elseif(function_exists("save_signup_entry")){
+		save_signup_entry();
+	}
+	else {
+		print "ERROR: Invalid Action `{$action}`.";
+
+	}
+}
+else {
+	print "ERROR: Please supply an action.";
+}
+
+# ===================================================
+# THE END
+# ===================================================
